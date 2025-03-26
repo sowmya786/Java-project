38dc4def6f532dc4f242dca0a9aeee37658e74b2
sowmya786 <b.sowmya998@gmail.com>
2025-03-26 15:18:47 +0530

    first commit

--- Lead/Java project/src/AddNumbers.java
+++ Lead/Java project/src/AddNumbers.java
@@ -2,7 +2,13 @@
 public class AddNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("eat something");
+		}
 
 	}
 
